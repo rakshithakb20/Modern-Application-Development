0699d9be18bd51c530d7e5f277766753c23d07aa
rakshithakb20 <94828867+rakshithakb20@users.noreply.github.com>
2021-12-26 09:57:42 +0530

    Add files via upload
    LAB1

---
+++ src/main/java/com/example/lab1a/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.lab1a;0
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView2 = findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("textsend");
+        textView2.setText(text);
+    }
+}
